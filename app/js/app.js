fbb90703c8f08255d2e6b7d54b2421029ccb4fd7
Elliot Wright <wright.elliot@gmail.com>
2014-03-20 12:06:10 +0000

    Added animated spinner.

--- app/js/app.js
+++ app/js/app.js
@@ -168,18 +168,41 @@ angular.module('wowpr.controllers', [])
     function($scope, $q, ApiClient, ConfigManager) {
       // Set up default config values here, other pages can redirect back to here
       // if they don't have sufficient data
+      var action = {};
+
       if ( ! ConfigManager.get('region')) {
         ConfigManager.set('region', 'eu');
       }
 
-      $scope.regions = [{'name': 'Europe', 'value': 'eu'}, {'name': 'United States', 'value': 'us'}];
+      // Set up spinner
+      _logo    = angular.element(document.getElementById('logo'));
+      _spinner = angular.element(document.getElementById('spinner'));
+
+      action.showSpinner = function() {
+        _logo.addClass('hide');
+        _spinner.addClass('active');
+      };
+
+      action.hideSpinner = function() {
+          _logo.removeClass('hide');
+          _spinner.removeClass('active');
+      };
+
+      $scope.regions = [
+        { 'name': 'Europe', 'value': 'eu' },
+        { 'name': 'United States', 'value': 'us' },
+        { 'name': 'Korea', 'value': 'kr' },
+        { 'name': 'Taiwan', 'value': 'tw' }
+      ];
       $scope.region  = ConfigManager.get('region');
       $scope.$watch('region', function() {
         ConfigManager.set('region', $scope.region);
 
-        // TODO: Begin region spinner
+        action.showSpinner();
+
         ApiClient.findRealms($scope.region).then(function(realms) {
-          // TODO: End region spinner
+
+          action.hideSpinner();
           $scope.realms = realms.realms;
 
           $scope.doSearch = function() {
